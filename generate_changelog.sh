91afda8c2c74cea51c6cd37486672d49b8b0916c
clicketz <40314230+clicketz@users.noreply.github.com>
2022-09-03 17:38:53 -0600

    Update generate_changelog.sh

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-version=$( git describe --tags --always )
-tag=$( git describe --tags --always --abbrev=0 )
-
-if [ "$version" = "$tag" ]; then # on a tag
-  current="$tag"
-  previous=$( git describe --tags --abbrev=0 HEAD~ )
-  if [[ $previous == *beta* ]]; then
-    if [[ $tag == *beta* ]]; then
-      previous=$( git describe --tags --abbrev=0 HEAD~ )
-    else
-      previous=$( git describe --tags --abbrev=0 --exclude="*beta*" HEAD~ )
-    fi
-  else
-    previous=$( git describe --tags --abbrev=0 HEAD~ )
-  fi
-else
-  current=$( git log -1 --format="%H" )
-  previous="$tag"
-fi
+previous=$( git describe --abbrev=0 --tags --exclude="$(git describe --abbrev=0 --tags)" )
+current=$( git describe --tags --always --abbrev=0 )
 
 date=$( git log -1 --date=short --format="%ad" )
 url=$( git remote get-url origin | sed -e 's/^git@\(.*\):/https:\/\/\1\//' -e 's/\.git$//' )
+commits=$( git log --pretty=format:"- "%s --no-merges --cherry-pick ${previous}...${current} )
 
-echo -ne "# [${version}](${url}/tree/${current}) ($date)\n\n[Full Changelog](${url}/compare/${previous}...${current})\n\n" > "CHANGELOG.md"
-
-if [ "$version" = "$tag" ]; then # on a tag
-  git cat-file -p "$tag" | sed -e '1,5d' -e '/^-----BEGIN PGP/,/^-----END PGP/d' >> "CHANGELOG.md"
-fi
+echo -ne "$commits" > "CHANGELOG.md"
+echo -e "[${current}](${url}/tree/${current}) ($date)\n\n[Full Changelog](${url}/compare/${previous}...${current})\n\n$(sort -u "CHANGELOG.md")" > "CHANGELOG.md"
