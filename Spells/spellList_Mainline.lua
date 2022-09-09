541f13c0beeef64171338500e1f5430a28e8b7bb
clicketz <40314230+clicketz@users.noreply.github.com>
2022-09-08 22:24:35 -0600

    Add Camouflage (Hunter)

--- Spells/spellList_Mainline.lua
+++ Spells/spellList_Mainline.lua
@@ -41,6 +41,7 @@ BuffOverlay.defaultSpells = {
     [53480] = { class = "HUNTER", prio = 50 }, --Roar of Sacrifice
     [264735] = { class = "HUNTER", prio = 50 }, --Survival of the Fittest (Pet Ability)
     [281195] = { parent = 264735 }, --Survival of the Fittest (Lone Wolf)
+    [199483] = { class = "HUNTER", prio = 70 }, --Camouflage
 
     -- Mage
     [45438] = { class = "MAGE", prio = 10 }, --Ice Block
