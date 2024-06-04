b78b9823de47b4b46bf82b73ade8885a41454bd1
clicketz <40314230+clicketz@users.noreply.github.com>
2024-06-04 02:14:12 -0600

    Add project id check for classic spell list

--- Spells/Vanilla.lua
+++ Spells/Vanilla.lua
@@ -1,3 +1,5 @@
+if WOW_PROJECT_ID ~= WOW_PROJECT_CLASSIC then return end
+
 ---@class BuffOverlay: AceModule
 local BuffOverlay = LibStub("AceAddon-3.0"):GetAddon("BuffOverlay")
 local L = BuffOverlay.L
