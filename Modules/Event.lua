150edfed64abf24e6210f113554ac2fca861d5f9
clicketz <40314230+clicketz@users.noreply.github.com>
2024-06-10 02:24:54 -0600

    Create Event module

---
+++ Modules/Event.lua
@@ -0,0 +1,17 @@
+local addonName = ...
+
+---@class BuffOverlay: AceAddon
+local Addon = LibStub('AceAddon-3.0'):GetAddon(addonName)
+
+---@class Event: AceModule
+local Event = Addon:NewModule('Event')
+
+---@class Aura: AceModule
+local Aura = Addon:GetModule('Aura')
+
+-- For Blizzard Frames
+hooksecurefunc("CompactUnitFrame_UpdateAuras", function(frame)
+    if not frame.buffFrames then return end
+
+    Aura:Update(frame, frame.displayedUnit)
+end)
