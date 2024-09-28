e527d0e327bcb22e1ec3931f78ec8c85c65906e9
clicketz <40314230+clicketz@users.noreply.github.com>
2024-09-28 17:09:27 +0000

    Add Food and Drink for different locales

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -15,4 +15,5 @@ L["Food"] = GetSpellInfo(5004) or "Food"
 local newFood = GetSpellInfo(369156) or L["Food"] -- Some locales have different names for "food" in recent patches.
 L["NewFood"] = newFood ~= L["Food"] and newFood or "Remove"
 L["Food & Drink"] = GetSpellInfo(170906) or "Food & Drink"
+L["Food and Drink"] = GetSpellInfo(462177) or "Food and Drink"
 L["Refreshment"] = GetSpellInfo(44166) or "Refreshment"
