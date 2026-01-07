49da77b649d9e777b09576fe2751c7bf08c0cba7
clicketz <40314230+clicketz@users.noreply.github.com>
2026-01-06 23:19:05 -0700

    [retail] Add Ancient of Lore

--- Spells/Mainline.lua
+++ Spells/Mainline.lua
@@ -38,6 +38,7 @@ BuffOverlay.defaultSpells = {
     [203554] = { class = "DRUID", prio = 5 },  --Focused Growth
         [347621] = { parent = 203554 },
     [362486] = { class = "DRUID", prio = 10 }, --Tranquility (Druid PVP)
+    [473909] = { class = "DRUID", prio = 50 }, --Ancient of Lore
     [22842] = { class = "DRUID", prio = 50 },  --Frenzied Regeneration
     [102342] = { class = "DRUID", prio = 50 }, --Ironbark
     [22812] = { class = "DRUID", prio = 50 },  --Barkskin
