0034cd8765f7692c9ec0e621881a01e46e3ad078
clicketz <40314230+clicketz@users.noreply.github.com>
2026-06-07 09:39:33 -0600

    Add a helper file

---
+++ Helpers.lua
@@ -0,0 +1,37 @@
+---@class BuffOverlay: AceModule
+local BuffOverlay = LibStub("AceAddon-3.0"):GetAddon("BuffOverlay")
+
+function BuffOverlay.CreateAuraButton(name)
+    local f = CreateFrame("Button", name, UIParent)
+    f:Hide()
+
+    f.icon = f:CreateTexture(nil, "BACKGROUND")
+    f.icon:SetAllPoints()
+
+    f.normal = f:CreateTexture(nil, "BORDER")
+    f.normal:SetAllPoints()
+    f.normal:SetTexture(nil)
+
+    -- pushed texture (unused atm)
+    f.pushed = f:CreateTexture(nil, "ARTWORK")
+    f.pushed:SetAllPoints()
+    f.pushed:SetTexture(nil)
+    f:SetPushedTexture(f.pushed)
+
+    -- highlight texture (unused atm)
+    f.highlight = f:CreateTexture(nil, "HIGHLIGHT")
+    f.highlight:SetAllPoints()
+    f.highlight:SetTexture(nil)
+    f:SetHighlightTexture(f.highlight)
+
+    -- cooldown spiral
+    f.cooldown = CreateFrame("Cooldown", "$parentCooldown", f, "CooldownFrameTemplate")
+    f.cooldown:SetAllPoints()
+    f.cooldown:SetDrawEdge(false)
+
+    -- stack count
+    f.count = f:CreateFontString(nil, "OVERLAY", "Number12Font")
+    f.count:SetPoint("BOTTOMRIGHT", 0, 2)
+
+    return f
+end
